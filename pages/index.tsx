02d01bff725d2dbea749dfddff73385195fb66d3
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-02-18 22:12:37 -0800

    Initial commit.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import Search from "../icons/search.svg";
+
+const IconsPage: NextPage = () => (
+  <div>
+    <h1>Icons</h1>
+    <Search />
+  </div>
+);
+
+export default IconsPage;
